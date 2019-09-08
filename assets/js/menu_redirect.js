4e2bc87607a7eb0c1e26d7735c22da0e1fbed9c5
unknown <dtroutma@stetson.edu>
2019-09-08 16:40:22 -0400

    Grammer Updates and Logo Redirect functionality

--- assets/js/menu_redirect.js
+++ assets/js/menu_redirect.js
@@ -2,30 +2,33 @@ var home = document.getElementById('home_page');
 var about = document.getElementById('about_page');
 var portfolio = document.getElementById('portfolio_page');
 var contact = document.getElementById('contact_page');
+var logo = document.getElementById('logo');
 
 
-
-if( home != undefined)  //Encrypt Message in Image
+if( home != undefined)  //Redirects Home
 {
     home.addEventListener("click", redirect_home, false);
 }
  
-if( about != undefined)    //Encrypt Image inside of Image
+if( about != undefined)    //Redirects to About
 {
     about.addEventListener("click", redirect_about, false);
 }
  
-if( portfolio != undefined)    //Decrypt Message inside of Image
+if( portfolio != undefined)    //Redirects to Portfolio
 {
     portfolio.addEventListener("click", redirect_portfolio, false);
 }
  
-if( contact != undefined)    //Decrypt image inside of Image
+if( contact != undefined)    //Redirects to Contact
 {
     contact.addEventListener("click", redirect_contact, false);
 }
  
-
+if( logo != undefined)    //Redirects home
+{
+   logo.addEventListener("click", redirect_home, false);
+}
 
 
 function redirect_home(e) 
